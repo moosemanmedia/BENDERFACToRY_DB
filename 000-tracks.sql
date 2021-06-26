80fb9a908141e2bb2905bc0ffbc424dc123ba16b
Mikkel Højgaard Sørensen <mikkel@mooseman.media>
2021-06-27 00:23:54 +0200

    Just starting now. New to git so give me a minute to figure this out

--- 000-tracks.sql
+++ 000-tracks.sql
@@ -1,20 +1,19 @@
 create table if not exists tracks
 (
-	id serial not null
-		constraint tracks_pk
-			primary key,
-	hash varchar(128) not null,
-	name varchar(128) not null
+    id   serial       not null
+        constraint tracks_pk
+            primary key,
+    hash varchar(128) not null,
+    name varchar(128) not null
 );
 
-alter table tracks owner to postgres;
-
 create unique index if not exists tracks_id_uindex
-	on tracks (id);
+    on tracks (id);
 
 create unique index if not exists tracks_hash_uindex
-	on tracks (hash);
+    on tracks (hash);
 
 create unique index if not exists tracks_name_uindex
-	on tracks (name);
+    on tracks (name);
+
 
